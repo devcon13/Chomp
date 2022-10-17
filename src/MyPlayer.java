6dc4bebc65602af006e50482c25ed0ce8f9bf129
Devon <devon_connerly24@milton.edu>
2022-10-17 12:28:37 -0400

    I drew a diagram of the possible outcomes for each number combination to try to figure out the best strategy for Chomp.

---
+++ src/MyPlayer.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+
+public class MyPlayer {
+    public Chip[][] gameBoard;
+    public int[] columns;
+
+    public MyPlayer() {
+        columns = new int[10];
+
+        /***
+         * This code will run just once, when the game opens.
+         * Add your code here.
+         */
+
+        for(int x=3; x>=1; x--){ // first number
+            for(int y=x; y>=0; y--){ // second number
+                for(int z=y; z>=0; z--){ // third number
+                    System.out.println("("+x+" "+y+" "+z+")");
+                }
+            }
+
+        }
+    }
+
+    public Point move(Chip[][] pBoard) {
+
+        System.out.println("MyPlayer Move");
+
+        gameBoard = pBoard;
+        int column = 0;
+        int row = 0;
+
+        row = 1;
+        column = 1;
+
+        /***
+         * This code will run each time the "MyPlayer" button is pressed.
+         * Add your code to return the row and the column of the chip you want to take.
+         * You'll be returning a data type called Point which consists of two integers.
+         */
+
+        Point myMove = new Point(row, column);
+        return myMove;
+    }
+
+}

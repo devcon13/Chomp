15d0c85e5951a4b6e2706d81f521beafded082c5
Devon <devon_connerly24@milton.edu>
2022-10-22 08:43:44 -0400

    I printed out the possible boards and their outcomes.

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -3,6 +3,8 @@ import java.awt.*;
 public class MyPlayer {
     public Chip[][] gameBoard;
     public int[] columns;
+    public int num1, num2, num3;
+    public boolean WS;
 
     public MyPlayer() {
         columns = new int[10];
@@ -12,14 +14,73 @@ public class MyPlayer {
          * Add your code here.
          */
 
-        for(int x=3; x>=1; x--){ // first number
-            for(int y=x; y>=0; y--){ // second number
-                for(int z=y; z>=0; z--){ // third number
-                    System.out.println("("+x+" "+y+" "+z+")");
+        for (int x = 3; x >= 1; x--) { // first number
+            for (int y = x; y >= 0; y--) { // second number
+                for (int z = y; z >= 0; z--) { // third number
+                    System.out.println();
+                    System.out.print("(" + x + " " + y + " " + z + ") --> ");
+
+                    for (int c = 1; c <= z; c++) { // LVL 1
+                        num1 = x;
+                        num2 = y;
+                        num3 = z-c;
+                        System.out.print(num1 + "" + num2 + "" + num3 + ", ");
+                    }
+                    if(num1==1 && num2==0 && num3==0){
+                        WS=true;
+                    }
+
+
+                    for (int b=1; b<=y; b++){ // LVL 2
+                        if(y-b>z){
+                            num1 = x;
+                            num2 = y-b;
+                            num3 = z;
+                            System.out.print(num1 + "" + num2 + "" + num3 + ", ");
+                        }
+                        else {
+                            num1 = x;
+                            num2 = y-b;
+                            num3 = y-b;
+                            System.out.print(num1 + "" + num2 + "" + num3 + ", ");
+                        }
+                    }
+                    if(num1==1 && num2==0 && num3==0){
+                        WS=true;
+                    }
+
+
+                    for (int a=1; a<=x-1; a++){ // LVL 3
+                        if(x-a>y){
+                            num1 = x-a;
+                            num2 = y;
+                            num3 = y;
+                            System.out.print(num1 + "" + num2 + "" + num3 + ", ");
+                        }
+                        if(x-a>z && x-a<=y){
+                            num1 = x-a;
+                            num2 = x-a;
+                            num3 = z;
+                            System.out.print(num1 + "" + num2 + "" + num3 + ", ");
+                        }
+                        else {
+                            num1 = x-a;
+                            num2 = x-a;
+                            num3 = x-a;
+                            System.out.print(num1 + "" + num2 + "" + num3 + ", ");;
+                        }
+                        if(num1==1 && num2==0 && num3==0){
+                            WS=true;
+                        }
+                    }
+                    if(WS==true){
+                        System.out.print(" WS");
+                        WS = false;
+                    }
                 }
             }
-
         }
+        System.out.println(" LS");
     }
 
     public Point move(Chip[][] pBoard) {
